2c38d95f8f7455e39173694610a3078962f39480
Naman <namankhetwal99@gmail.com>
2024-09-24 14:11:08 +0530

    Display the quantity number of product in checkout and amazon page

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from '../data/cart.js';
+import { cart, addToCart, calculateCartQuantity } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -65,18 +65,13 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 
 function UpdateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-  console.log(cartQuantity);
-  console.log(cart);
-
+  const cartQuantity = calculateCartQuantity();
 
   document.querySelector('.js-cart-quantity')
     .innerHTML = cartQuantity;
 }
+
+UpdateCartQuantity();
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
